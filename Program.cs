d3fb61443ec06b4e4191bc87ac224422de635949
Anthony Cherry <mrcherry97@gmail.com>
2022-07-01 10:57:07 -0400

    completed overload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SolrNet.Utils;
+
+Console.WriteLine("Hello, World!");
+
+static int Add(int x, int y)
+{
+    return x + y;
+}
+static double Add(double x, double y) 
+{
+    return x + y;
+}
+
+static string Add(int x, int y, bool IsTrue);
+
+var sum = X + y;
+
+if(sum == 1.00) 
+{
+        return sum ("dollar");
+}
+if(sum > 1.00) 
+{
+    return sum ("dollars");
+}
+else 
+{
+    return sum;
+}
